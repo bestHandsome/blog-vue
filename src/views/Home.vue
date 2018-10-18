f9f187217b8fc0291e7a4d2fb1ef6f5e1780c161
xingjin <xingjin@fayfans.com>
2018-10-18 10:04:09 +0800

    删除不必要代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -100,36 +100,6 @@
         </v-layout>
       </v-parallax>
     </section>
-    <template>
-      <div class="text-xs-center">
-        <v-bottom-sheet v-model="sheet">
-          <v-btn
-                  slot="activator"
-                  color="purple"
-                  dark>
-            Click me
-          </v-btn>
-
-          <v-list>
-            <v-subheader>Open in</v-subheader>
-            <v-list-tile
-                    v-for="tile in tiles"
-                    :key="tile.title"
-                    @click="sheet = false">
-              <v-list-tile-avatar>
-                <v-avatar size="32px" tile>
-                  <img
-                          :src="`https://cdn.vuetifyjs.com/images/bottom-sheets/${tile.img}`"
-                          :alt="tile.title"
-                  >
-                </v-avatar>
-              </v-list-tile-avatar>
-              <v-list-tile-title>{{ tile.title }}</v-list-tile-title>
-            </v-list-tile>
-          </v-list>
-        </v-bottom-sheet>
-      </div>
-    </template>
   </div>
 </template>
 
@@ -155,7 +125,7 @@ export default {
       }
   },
   methods: {
-      onScroll (e) {
+      onScroll () {
           this.offsetTop = window.pageYOffset || document.documentElement.scrollTop;
       }
   }
